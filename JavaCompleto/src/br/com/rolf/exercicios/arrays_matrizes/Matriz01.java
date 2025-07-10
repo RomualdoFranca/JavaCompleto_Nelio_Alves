6595c7d4b50481068fb7f2d01fc0707667725b6c
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-10 09:30:31 -0300

    cria uma matriz. implenta dois lacos, um para inserir os valores e o outro para imprimi-los

---
+++ JavaCompleto/src/br/com/rolf/exercicios/arrays_matrizes/Matriz01.java
@@ -0,0 +1,36 @@
+package br.com.rolf.exercicios.arrays_matrizes;
+
+import java.util.Scanner;
+
+public class Matriz01 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		//cria uma matriz e define o tamanho
+		int[][] matriz01 = new int[2][2];
+		
+		//laco para inserir os valores na matriz manualmente
+		for (int i = 0; i < matriz01.length; i++) {
+			for (int j = 0; j < matriz01[i].length; j++) {
+				System.out.println("Digite os numeros da matriz");
+				int num = sc.nextInt();
+				sc.nextLine();
+				matriz01[i][j] = num;
+			}
+		}
+		
+		//imprime os valores
+		for (int i = 0; i < matriz01.length; i++) {
+			for (int j = 0; j < matriz01[i].length; j++) {
+				System.out.print(matriz01[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		sc.close();
+	}
+
+}

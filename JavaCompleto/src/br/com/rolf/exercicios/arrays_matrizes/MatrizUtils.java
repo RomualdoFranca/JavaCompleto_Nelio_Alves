534da216943fdc8be3027d5b45b698594872ca87
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-13 19:31:00 -0300

    Cria metodo para pesquisaar numero. Refatora os seguintes metodos: retornaIndices(): retira o Scanner do parametros, retira o input de pesquisa do numero de dentro do metodo. estaNoVortice(): retira o Scanner do parametros. Acrescenta dois parametros int para armazenar os indices do numero pesquisado

--- JavaCompleto/src/br/com/rolf/exercicios/arrays_matrizes/MatrizUtils.java
+++ JavaCompleto/src/br/com/rolf/exercicios/arrays_matrizes/MatrizUtils.java
@@ -81,7 +81,54 @@ public final class MatrizUtils {
 	}
 
 	// MÁTODOS PARA PESQUISAS
+	
+	public static int selecionaNumero(Scanner sc) {
+		System.out.println("Digite um número");
+		int num = sc.nextInt();
+		sc.nextLine();
+		return num;
+	}
 
+	// MÉTODOS PARA LOCALIZAR ELEMENTOS
+
+	// verificar se o numero pesquisado esta nos vertices da martriz
+	public static void estaNoVertice(int[][] matriz, Scanner sc) {
+
+		int ultimaColunaLinha = matriz.length - 1;
+		int[] vectIndice = MatrizUtils.retornaIndices(matriz, sc);
+
+		// se a condicao for true, o numero ta no topo da matriz
+		if (vectIndice[0] == 0) {
+			// verifica se esta nos vértices da esquerda ou direita
+			if (vectIndice[1] == 0) {
+				System.out.println("O número está no vértice esquerdo no topo da matriz");
+			} else if (vectIndice[1] == ultimaColunaLinha) {
+				System.out.println("O número está no vértice direito no topo da matriz");
+
+			} else {
+				// para uma futura implementação: retornar a coluna em que o numero se encontra
+				System.out.println("O número está no topo, entre os vértices");
+			}
+
+			// indica que o numero esta na base da martiz
+		} else if (vectIndice[0] == ultimaColunaLinha) {
+			// verifica se esta nos vértices da esquerda ou direita
+			if (vectIndice[1] == 0) {
+				System.out.println("O número está no vértice esquerdo na base da matriz");
+			} else if (vectIndice[1] == ultimaColunaLinha) {
+				System.out.println("O número está no vértice direito na base da matriz");
+
+			} else {
+				System.out.println("O número está na base, entre os vértices");
+			}
+
+		} else {
+			System.out.println("O número não está nos extremos da matriz");
+		}
+	}
+
+	
+	
 	public static int[] retornaIndices(int[][] matriz, Scanner sc) {
 		System.out.println("Digite numero");
 		int num = sc.nextInt();
@@ -271,29 +318,5 @@ public final class MatrizUtils {
 		}
 		return count;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
--- JavaCompleto/src/br/com/rolf/experimentos/temporarios/PesquisasMatriz.java
+++ JavaCompleto/src/br/com/rolf/experimentos/temporarios/PesquisasMatriz.java
@@ -17,11 +17,18 @@ public class PesquisasMatriz {
 		
 //		MatrizUtils.insereNumerosAleatorios(m);
 		MatrizUtils.imprimeSaida(m);
-//		int frequencia = MatrizUtils.contaFrequenciaNumero(m, sc);
-//		System.out.println(frequencia);
 
-		estaNoVertice(m, sc);
-//		System.out.println(MatrizUtils.contaFrequencia(m, sc));
+		int num = selecionaNumero(sc);
+		int[] indices = retornaIndices(m, num);
+		System.out.print(indices[0] + ", " + indices[1] + "\n");
+		
+		int[] numerosContiguos  = retornaNumerosVizinhosVerticeEsquerdoTopo(m);
+		System.out.println("Numero à direita: " + numerosContiguos[0] +
+				"\nNúmero abaixo: " + numerosContiguos[1]);
+		
+//		estaNoVertice(m, indices[0], indices[1]);
+		
+//		MatrizUtils.estaNoVertice(m, sc);
 		
 		sc.close();
 		
@@ -34,45 +41,99 @@ public class PesquisasMatriz {
 		
 	}
 	
-// identificar qual linha ou coluna o numero pesquisado se encontra
+	
+	public static int selecionaNumero(Scanner sc) {
+		System.out.println("Digite um número");
+		int num = sc.nextInt();
+		sc.nextLine();
+		return num;
+	}
 	
 	
-	//verificar se o numero pesquisado esta nos vertices da martriz
+	//retorna os indices dos numeros vizinhos do vertice correspondente
 	
-	public static void estaNoVertice(int[][] matriz, Scanner sc) {
+	public static int[] retornaNumerosVizinhosVerticeEsquerdoTopo(int[][] matriz) {
 		
-		int ultimaColunaLinha = matriz.length - 1;
-		int[] vectIndice = MatrizUtils.retornaIndices(matriz, sc);
+		int[] numerosContiguos = new int[2];
+		//numerosContiguos[0] = numero da direita
+		//numerosContiguos[1] = numero abaixo
 		
-		//se a condicao for true, o numero ta no topo da matriz
-		if (vectIndice[0] == 0) {
-		//verifica se esta na esquerda ou direita
-			if (vectIndice[1] == 0) {
-				System.out.println("O número está no vértice esquerdo no topo da matriz");
-			}else if (vectIndice[1] == ultimaColunaLinha) {
-				System.out.println("O número está no vértice direito no topo da matriz");
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
 				
-			}else {
-				//para uma futura implementação: retornar a coluna em que o numero se encontra
-				System.out.println("O número está no topo, entre os vértices");
+				if (i == 0 && j == 1) {//numero da direita
+					numerosContiguos[0] = matriz[i][j];
+					
+				}else if (i == 1 && j == 0) {//numero abaixo
+					numerosContiguos[1] = matriz[i][j];
+				}
 			}
-			
-			// indica que o numero esta na base da martiz
-		}else if (vectIndice[0] == ultimaColunaLinha) {
-			//verifica se esta na esquerda ou direita
-			if (vectIndice[1] == 0) {
-				System.out.println("O número está no vértice esquerdo na base da matriz");
-			}else if (vectIndice[1] == ultimaColunaLinha) {
-				System.out.println("O número está no vértice direito na base da matriz");
-				
-			}else {
-				System.out.println("O número está na base, entre os vértices");
-			}
-			
-		}else {
-			System.out.println("O número não está nos extremos da matriz");
 		}
 		
+		return numerosContiguos;
+	
+		
+	}
+	
+	
+	public static void estaNoVertice(int[][] matriz,int linha, int coluna) {
+
+		int ultimaColunaLinha = matriz.length - 1;
+		int[] vectIndice = new int[] {linha, coluna};
+
+		// se a condicao for true, o numero ta no topo da matriz
+		if (vectIndice[0] == 0) {
+			// verifica se esta nos vértices da esquerda ou direita
+			if (vectIndice[1] == 0) {
+				System.out.println("O número está no vértice esquerdo no topo da matriz");
+				
+			} else if (vectIndice[1] == ultimaColunaLinha) {
+				System.out.println("O número está no vértice direito no topo da matriz");
+
+			} else {
+				// para uma futura implementação: retornar a coluna em que o numero se encontra
+				System.out.println("O número está no topo, entre os vértices");
+			}
+
+			// indica que o numero esta na base da martiz
+		} else if (vectIndice[0] == ultimaColunaLinha) {
+			// verifica se esta nos vértices da esquerda ou direita
+			if (vectIndice[1] == 0) {
+				System.out.println("O número está no vértice esquerdo na base da matriz");
+				
+			} else if (vectIndice[1] == ultimaColunaLinha) {
+				System.out.println("O número está no vértice direito na base da matriz");
+
+			} else {
+				System.out.println("O número está na base, entre os vértices");
+			}
+
+		} else {
+			System.out.println("O número não está nos extremos da matriz");
+		}
+	}
+
+	
+	public static int[] retornaIndices(int[][] matriz, int numeroPesquisado) {
+		
+		int[] vect = new int[2];
+
+		boolean temNumero = MatrizUtils.verificaExistenciaNumero(numeroPesquisado, false, matriz);
+		if (temNumero == true) {
+
+			for (int i = 0; i < matriz.length; i++) {
+				for (int j = 0; j < matriz[i].length; j++) {
+					if (matriz[i][j] == numeroPesquisado) {
+						vect[0] = i;
+						vect[1] = j;
+						return vect;
+					}
+				}
+			}
+		} else {
+			System.out.println("Número inexistente");
+		}
+		return vect;
 	}
 	
 	

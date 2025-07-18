27822632508b78fb31115a8a013b56abbc59c975
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-18 10:07:13 -0300

    implementa metodo que concentra todos os metodos de retorno, tanto dos vertices quanto das bordas

--- JavaCompleto/src/br/com/rolf/exercicios/arrays_matrizes/MatrizUtilsRefactory.java
+++ JavaCompleto/src/br/com/rolf/exercicios/arrays_matrizes/MatrizUtilsRefactory.java
@@ -6,29 +6,26 @@ import java.util.Scanner;
 import br.com.rolf.utilidades.EntradaUtils;
 
 public final class MatrizUtilsRefactory {
-	
-	private  MatrizUtilsRefactory() {
+
+	private MatrizUtilsRefactory() {
 	}
 
-	//_____________________________________________________________________//
-	//_____________________________________________________________________//
-	
-	
+	// _____________________________________________________________________//
+	// _____________________________________________________________________//
 
-	
-	//MÉTODOS DE CRIAÇÃO DE MATRIZES
-	
+	// MÉTODOS DE CRIAÇÃO DE MATRIZES
+
 	public static int[] retornaNumeroLinhaColuna() {
-		
+
 		int linha = EntradaUtils.lerInteiro("Digite o número de linha da matriz");
 		int coluna = EntradaUtils.lerInteiro("Digite o número de coluna da matriz");
-		
-		return new int[] {linha, coluna};
+
+		return new int[] { linha, coluna };
 	}
-	//_____________________________________________________________________//
-	
+	// _____________________________________________________________________//
+
 	public static int[][] criaMatriz() {
-		
+
 		int[][] matriz = null;
 		int[] numeroLinhaColuna = retornaNumeroLinhaColuna();
 		matriz = new int[numeroLinhaColuna[0]][numeroLinhaColuna[1]];
@@ -36,37 +33,36 @@ public final class MatrizUtilsRefactory {
 		return matriz;
 	}
 
-	//_____________________________________________________________________//
-	
+	// _____________________________________________________________________//
+
 	public static int[][] criaMatrizDenteada() {
-		
+
 		int[][] matrizDenteada = null;
-		
+
 		int linha = EntradaUtils.lerInteiro("Digite o número de linha");
 		matrizDenteada = new int[linha][];
-		
+
 		int coluna = 0;
-		
+
 		for (int i = 0; i < matrizDenteada.length; i++) {
 			coluna = EntradaUtils.lerInteiro("Digite o número de coluna da " + (i + 1) + "° linha");
 
 			matrizDenteada[i] = new int[coluna];
 		}
-		
+
 		return matrizDenteada;
 	}
-	
-	//_____________________________________________________________________//
-	//_____________________________________________________________________//
-	
-	
-	//MÉTODOS DE PESQUISA
-	
+
+	// _____________________________________________________________________//
+	// _____________________________________________________________________//
+
+	// MÉTODOS DE PESQUISA
+
 	public static int[] retornaIndices(int[][] matriz) {
-		
+
 		int numeroPesquisado = EntradaUtils.lerInteiro("Digite o número para pesquisa");
-		int[] indices = new int [2];
-		
+		int[] indices = new int[2];
+
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
 				if (matriz[i][j] == numeroPesquisado) {
@@ -78,15 +74,12 @@ public final class MatrizUtilsRefactory {
 
 		return indices;
 	}
-	//_____________________________________________________________________//
-	
-	
-	
+	// _____________________________________________________________________//
+
 	public static int[] retornaNumerosVizinhosVertices(int[][] matriz, int linha, int coluna) {
 		int[] numVizinhos = new int[4];
 		int ultimaLinhaColuna = matriz.length - 1;
-		
-		
+
 		int numMaxCol = matriz[0].length;
 //		//loops para retornar o número máximo de colunas
 //		for (int i = 0; i < matriz.length; i++) {
@@ -95,68 +88,130 @@ public final class MatrizUtilsRefactory {
 //			}
 //			break; // interrompe o loop interno depois do primeiro ciclo
 //		}
-		
-		//numero pesquisado esta no vertice superior esquerdo
+
+		// numero pesquisado esta no vertice superior esquerdo
 		if (linha + coluna == 0) {
-			numVizinhos[0] = matriz[linha + 1][coluna];//vizinho abaixo
-			numVizinhos[1] = matriz[linha][coluna + 1];//vizinho a direita
+			numVizinhos[0] = matriz[linha + 1][coluna];// vizinho abaixo
+			numVizinhos[1] = matriz[linha][coluna + 1];// vizinho a direita
 //			EntradaUtils.lerVizinhos(" Número Abaixo: %d\nNúmero à Direita: %d", numVizinhos[0], numVizinhos[1]);
-			
+
 			EntradaUtils.lerVizinhos("""
+					Número pesquisado está no vértice superior esquerdo
 					Vizinho Abaixo: %d
 					Vizinho à Direita: %d
 					""".formatted(numVizinhos[0], numVizinhos[1]));
-			
-		//o numero pesquisado esta no vertice superior direito
-		}else if (linha + coluna == coluna) {
-			numVizinhos[0] = matriz[linha][coluna - 1];//vizinho esquerda
-			numVizinhos[1] = matriz[linha + 1][coluna];//vizinho abaixo
-			
+
+			// o numero pesquisado esta no vertice superior direito
+		} else if (linha + coluna == coluna) {
+			numVizinhos[0] = matriz[linha][coluna - 1];// vizinho esquerda
+			numVizinhos[1] = matriz[linha + 1][coluna];// vizinho abaixo
+
 			EntradaUtils.lerVizinhos("""
+					Número pesquisado está no vértice superior direito
 					Vizinho à Esquerda: %d
 					Vizinho Abaixo: %d
 					""".formatted(numVizinhos[0], numVizinhos[1]));
-			
-		// o numero pesquisado esta no vertice inferior esquerdo
-		}else if (linha + coluna == linha) {
-			numVizinhos[0] = matriz[linha -1 ][coluna];//vizinho acima
-			numVizinhos[1] = matriz[linha][coluna + 1];//vizinho a direita
-			
+
+			// o numero pesquisado esta no vertice inferior esquerdo
+		} else if (linha + coluna == linha) {
+			numVizinhos[0] = matriz[linha - 1][coluna];// vizinho acima
+			numVizinhos[1] = matriz[linha][coluna + 1];// vizinho a direita
+
 			EntradaUtils.lerVizinhos("""
+					Número pesquisado está no vértice inferior esquerdo
 					Vizinho Acima: %d
 					Vizinho à Direita: %d
 					""".formatted(numVizinhos[0], numVizinhos[1]));
-			
-		// o numero pesquisado esta no vertice inferior direito
-		}else if (linha == ultimaLinhaColuna && coluna == numMaxCol - 1 ) {
-			numVizinhos[0] = matriz[linha - 1][coluna];//vizinho acima
-			numVizinhos[1] = matriz[linha][coluna - 1];//vizinho a esquerda
-			
+
+			// o numero pesquisado esta no vertice inferior direito
+		} else if (linha == ultimaLinhaColuna && coluna == numMaxCol - 1) {
+			numVizinhos[0] = matriz[linha - 1][coluna];// vizinho acima
+			numVizinhos[1] = matriz[linha][coluna - 1];// vizinho a esquerda
+
 			EntradaUtils.lerVizinhos("""
+					Número pesquisado está no vértice inferior direito
 					Vizinho Acima: %d
 					Vizinho à Esquerda: %d
 					""".formatted(numVizinhos[0], numVizinhos[1]));
-			
+
 		}
 
 		return numVizinhos;
 	}
+	// _____________________________________________________________________//
+
+	public static int[] retornaVizinhosBordas(int[][] matriz, int linha, int coluna) {
+
+		int[] numVizinho = new int[4];
+		int numMaxCol = matriz[0].length;
+
+		boolean bordaSuperior = linha == 0 && (coluna < numMaxCol && coluna > 0);
+		boolean bordaInferior = linha == matriz.length - 1 && (coluna < numMaxCol && coluna > 0);
+		boolean bordaEsquerda = (linha < matriz.length - 1 && linha > 0) && coluna == 0;
+		boolean bordaDireita = (linha < matriz.length - 1 && linha > 0) && coluna < numMaxCol;
+
+		if (bordaSuperior) {
+			numVizinho[0] = matriz[linha][coluna - 1];// esquerda
+			numVizinho[1] = matriz[linha][coluna + 1];// direita
+			numVizinho[2] = matriz[linha + 1][coluna];// abaixo
+
+			EntradaUtils.lerVizinhos("""
+					Número pesquisado está na borda superior
+					Vizinho Esquerda: %d
+					Vizinho à Direita: %d
+					Vizinho Abaixo: %d
+					""".formatted(numVizinho[0], numVizinho[1], numVizinho[2]));
+		} else if (bordaInferior) {
+			numVizinho[0] = matriz[linha][coluna - 1];// esquerda
+			numVizinho[1] = matriz[linha][coluna + 1];// direita
+			numVizinho[2] = matriz[linha - 1][coluna];// acima
+
+			EntradaUtils.lerVizinhos("""
+					Número pesquisado está na borda inferior
+					Vizinho Esquerda: %d
+					Vizinho à Direita: %d
+					Vizinho Acima: %d
+					""".formatted(numVizinho[0], numVizinho[1], numVizinho[2]));
+		} else if (bordaEsquerda) {
+			numVizinho[0] = matriz[linha - 1][coluna];// acima
+			numVizinho[1] = matriz[linha + 1][coluna];// abaixo
+			numVizinho[2] = matriz[linha][coluna + 1];// direita
+
+			EntradaUtils.lerVizinhos("""
+					Número pesquisado está na borda à esquerda
+					Vizinho Acima: %d
+					Vizinho Abaixo: %d
+					Vizinho à Direita: %d
+					""".formatted(numVizinho[0], numVizinho[1], numVizinho[2]));
+		} else if (bordaDireita) {
+			numVizinho[0] = matriz[linha - 1][coluna];// acima
+			numVizinho[1] = matriz[linha + 1][coluna];// abaixo
+			numVizinho[2] = matriz[linha][coluna - 1];// esquerda
+
+			EntradaUtils.lerVizinhos("""
+					Número pesquisado está na borda à direita
+					Vizinho Acima: %d
+					Vizinho Abaixo: %d
+					Vizinho à Esquerda: %d
+					""".formatted(numVizinho[0], numVizinho[1], numVizinho[2]));
+		}
+
+		return numVizinho;
+	}
 	
-	
-	
-	
+	// _____________________________________________________________________//
+
 	// MÉTODOS PARA IMPRESSAO DE VETORES E MATRIZES
-	
-	
+
 	public static void imprimeVetor(int[] vect) {
 		for (int i = 0; i < vect.length; i++) {
 			System.out.print(vect[i] + " ");
 		}
 		System.out.println();
 	}
-	
-	//_____________________________________________________________________//
-	
+
+	// _____________________________________________________________________//
+
 	public static void imprimeMatriz(int[][] matriz) {
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
@@ -165,50 +220,33 @@ public final class MatrizUtilsRefactory {
 			System.out.println();
 		}
 	}
-	
-	//_____________________________________________________________________//
-	
+
+	// _____________________________________________________________________//
+
 //	MÉTODOS PARA PREENCHER MATRIZES E VETORES
-	
+
 	public static void preencheMatrizNumerosSequenciais(int[][] matriz) {
-		
+
 		int count = 1;
-		
+
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
 				matriz[i][j] = count++;
 			}
 		}
 	}
-	
-	//_____________________________________________________________________//
-	
+
+	// _____________________________________________________________________//
+
 	public static void preencheMatrizNumerosAleatorios(int[][] matriz, int min, int max) {
-		
+
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[i].length; j++) {
 				matriz[i][j] = (int) (Math.random() * (max - min + 1) + min);
 			}
 		}
 	}
-	
-	//_____________________________________________________________________//
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	// _____________________________________________________________________//
+
 }
--- JavaCompleto/src/br/com/rolf/experimentos/temporarios/PesquisasMatriz.java
+++ JavaCompleto/src/br/com/rolf/experimentos/temporarios/PesquisasMatriz.java
@@ -17,157 +17,170 @@ public class PesquisasMatriz {
 		MatrizUtilsRefactory.preencheMatrizNumerosSequenciais(m);
 		MatrizUtils.imprimeSaida(m);
 		int[] indices = MatrizUtilsRefactory.retornaIndices(m);
-		
-		System.out.println("O número pesquisado se encontra na\nlinha: " +
-				(indices[0]) + "\ncoluna:  " + (indices[1]));
-		
+
+		System.out.println("O número pesquisado se encontra na\nlinha: " + (indices[0]) + "\ncoluna:  " + (indices[1]));
+		System.out.println("matriz.lenght: " + m.length);
+
 //		int[] numerosVizinhos = MatrizUtilsRefactory.retornaNumerosVizinhosVertices(m,indices[0], indices[1]);
-		int[] vizinhosBordaSuperior = retornaVizinhosBordas(m, indices[0], indices[1]);
-		
+//		int[] vizinhosBordaSuperior = retornaVizinhosBordas(m, indices[0], indices[1]);
+
+		System.out.println("Metodo retorna vizinhos geral");
+		retornaVizinhos(m, indices[0], indices[1]);
+
 //		EntradaUtils.lerVizinhos("""
 //				
 //				""".formatted(args));
-		
+
 //		MatrizUtils.insereNumerosAleatorios(m);
-
-
 
 		sc.close();
 
-	}//fim do metodo main
-	
-	public static int[] retornaVizinhosBordas(int[][] matriz, int linha, int coluna) {
+	}// fim do metodo main
+
+	public static void retornaVizinhos(int[][] matriz, int linha, int coluna) {
+
+		int numMaxCol = matriz[0].length;
+
+		boolean numeroVertices = (linha == 0 || linha == matriz.length - 1) && (coluna == 0 || coluna == numMaxCol - 1);
+//		boolean numeroBordas = (linha >= matriz.length - 1 || linha >= 0) && (coluna < numMaxCol && coluna > 0);
 		
+		boolean numeroBordas = (linha == 0 && (coluna < numMaxCol && coluna > 0) //borda superior
+				|| linha == matriz.length - 1 && (coluna < numMaxCol && coluna > 0)//borda inferior
+				|| (linha < matriz.length - 1 && linha > 0) && coluna == 0 // borda esquerda
+				|| (linha < matriz.length - 1 && linha > 0) && coluna < numMaxCol); // borda direita
+
+		if (numeroVertices) {
+			MatrizUtilsRefactory.retornaNumerosVizinhosVertices(matriz, linha, coluna);
+		} else if (numeroBordas) {
+			MatrizUtilsRefactory.retornaVizinhosBordas(matriz, linha, coluna);
+		}
+
+	}
+
+	public static int[] retornaVizinhosBordas(int[][] matriz, int linha, int coluna) {
+
 		int[] numVizinho = new int[4];
 		int numMaxCol = matriz[0].length;
-		
+
 		boolean bordaSuperior = linha == 0 && (coluna < numMaxCol && coluna > 0);
 		boolean bordaInferior = linha == matriz.length - 1 && (coluna < numMaxCol && coluna > 0);
-		boolean bordaEsquerda = (linha < matriz.length -1 && linha > 0) && coluna == 0;
-		boolean bordaDireita  = (linha < matriz.length -1 && linha > 0) && coluna < numMaxCol;
-		
+		boolean bordaEsquerda = (linha < matriz.length - 1 && linha > 0) && coluna == 0;
+		boolean bordaDireita = (linha < matriz.length - 1 && linha > 0) && coluna < numMaxCol;
+
 		if (bordaSuperior) {
 			numVizinho[0] = matriz[linha][coluna - 1];// esquerda
-			numVizinho[1] = matriz[linha][coluna + 1];//direita
-			numVizinho[2] = matriz[linha + 1][coluna];//abaixo
-			
+			numVizinho[1] = matriz[linha][coluna + 1];// direita
+			numVizinho[2] = matriz[linha + 1][coluna];// abaixo
+
 			EntradaUtils.lerVizinhos("""
 					Vizinho Esquerda: %d
 					Vizinho à Direita: %d
 					Vizinho Abaixo: %d
 					""".formatted(numVizinho[0], numVizinho[1], numVizinho[2]));
-		}else if (bordaInferior) {
+		} else if (bordaInferior) {
 			numVizinho[0] = matriz[linha][coluna - 1];// esquerda
-			numVizinho[1] = matriz[linha][coluna + 1];//direita
-			numVizinho[2] = matriz[linha - 1][coluna];//acima
-			
+			numVizinho[1] = matriz[linha][coluna + 1];// direita
+			numVizinho[2] = matriz[linha - 1][coluna];// acima
+
 			EntradaUtils.lerVizinhos("""
 					Vizinho Esquerda: %d
 					Vizinho à Direita: %d
 					Vizinho Acima: %d
 					""".formatted(numVizinho[0], numVizinho[1], numVizinho[2]));
-		}else if (bordaEsquerda) {
-			numVizinho[0] = matriz[linha - 1][coluna];//acima
-			numVizinho[1] = matriz[linha + 1][coluna];//abaixo
-			numVizinho[2] = matriz[linha][coluna + 1];//direita
-			
+		} else if (bordaEsquerda) {
+			numVizinho[0] = matriz[linha - 1][coluna];// acima
+			numVizinho[1] = matriz[linha + 1][coluna];// abaixo
+			numVizinho[2] = matriz[linha][coluna + 1];// direita
+
 			EntradaUtils.lerVizinhos("""
 					Vizinho Acima: %d
 					Vizinho Abaixo: %d
 					Vizinho à Direita: %d
 					""".formatted(numVizinho[0], numVizinho[1], numVizinho[2]));
-		}else if (bordaDireita) {
-			numVizinho[0] = matriz[linha - 1][coluna];//acima
-			numVizinho[1] = matriz[linha + 1][coluna];//abaixo
-			numVizinho[2] = matriz[linha][coluna - 1];//esquerda
-			
+		} else if (bordaDireita) {
+			numVizinho[0] = matriz[linha - 1][coluna];// acima
+			numVizinho[1] = matriz[linha + 1][coluna];// abaixo
+			numVizinho[2] = matriz[linha][coluna - 1];// esquerda
+
 			EntradaUtils.lerVizinhos("""
 					Vizinho Acima: %d
 					Vizinho Abaixo: %d
 					Vizinho à Esquerda: %d
 					""".formatted(numVizinho[0], numVizinho[1], numVizinho[2]));
 		}
-		
-		
+
 		return numVizinho;
 	}
-	
-	
-	//retorna vizinhos numero do vertice esquerdo no topo da matriz
-	
+
+	// retorna vizinhos numero do vertice esquerdo no topo da matriz
+
 	public static int[] retornaNumerosVizinhosVerticeEsquerdoTopo(int[][] matriz, int linha, int coluna) {
 		int[] numVizinhos = null;
-		
+
 		if (linha + coluna == 0) {
 			numVizinhos = new int[matriz[linha + 1][coluna]];
 			numVizinhos = new int[matriz[linha][coluna + 1]];
 		}
-		
+
 		return numVizinhos;
 	}
-	
+
 	public static void escolheMetodo(int[][] matriz, int numeroPesquisado) {
-		
+
 		int[] indices = retornaIndices(matriz, numeroPesquisado);
-		//indices[0] correponde ao indice da linha da matriz
-		if (indices[0] == 0) {//o numero está na 1° linha
+		// indices[0] correponde ao indice da linha da matriz
+		if (indices[0] == 0) {// o numero está na 1° linha
 			System.out.println("Metodo do topo");
 			int[] numerosVizinhos = retornaNumerosVizinhosTopo(matriz, indices[0], indices[1]);
-			System.out.println("Numero  vizinho: " + numerosVizinhos[0] +
-					"\nNúmero vizinho: " + numerosVizinhos[1]);
-			
-		}else if (indices[0] == matriz.length - 1){//o numero está na última linha
+			System.out.println("Numero  vizinho: " + numerosVizinhos[0] + "\nNúmero vizinho: " + numerosVizinhos[1]);
+
+		} else if (indices[0] == matriz.length - 1) {// o numero está na última linha
 			System.out.println("Metodo da base");
 			int[] numerosVizinhos = retornaNumerosVizinhosBase(matriz, indices[0], indices[1]);
-			System.out.println("Numero  vizinho: " + numerosVizinhos[0] +
-					"\nNúmero vizinho: " + numerosVizinhos[1]);
-			
-		}else {//o numero está entre o topo e a base
+			System.out.println("Numero  vizinho: " + numerosVizinhos[0] + "\nNúmero vizinho: " + numerosVizinhos[1]);
+
+		} else {// o numero está entre o topo e a base
 			System.out.println("Numero está entre o topo e a base da matriz");
 			int[] numerosVizinhos = retornaNumerosVizinhosPrimeiraColunaEntreTopoBase(matriz, indices[0], indices[1]);
-			System.out.printf("Numero vizinho: %d\nNumero vizinho: %d\nNumero vizinho: %d\n",
-					numerosVizinhos[0], numerosVizinhos[1], numerosVizinhos[2]);
+			System.out.printf("Numero vizinho: %d\nNumero vizinho: %d\nNumero vizinho: %d\n", numerosVizinhos[0],
+					numerosVizinhos[1], numerosVizinhos[2]);
 			System.out.println();
-			
+
 		}
-		
-		
-		
+
 	}
-	
+
 	public static int[] retornaNumerosVizinhosPrimeiraColunaEntreTopoBase(int[][] matriz, int linha, int coluna) {
-		
+
 		int ultimaLinhaColuna = matriz.length - 1;
-		//variaveis com as posições dos numeros vizinhos
+		// variaveis com as posições dos numeros vizinhos
 		int numeroAcima = linha - 1;
 		int numeroAbaixo = linha + 1;
-		int numeroEsquerda = coluna -1;
+		int numeroEsquerda = coluna - 1;
 		int numeroDireita = coluna + 1;
-		
+
 		if (coluna == 0) {
 			for (int i = 0; i < matriz.length; i++) {
 				for (int j = 0; j < matriz[i].length; j++) {
 					int vizinhoAcima = matriz[numeroAcima][0];
 					int vizinhoAbaixo = matriz[numeroAbaixo][0];
 					int vizinhoDireita = matriz[linha][numeroDireita];
-					return new int[] { vizinhoAcima, vizinhoAbaixo, vizinhoDireita,  };
+					return new int[] { vizinhoAcima, vizinhoAbaixo, vizinhoDireita, };
 				}
 			}
 		}
-		
-		
+
 		return null;
 	}
-	
 
-	public static int[] retornaNumerosVizinhosTopo(int[][] matriz,int linha, int coluna) {
+	public static int[] retornaNumerosVizinhosTopo(int[][] matriz, int linha, int coluna) {
 
 		int ultimaLinhaColuna = matriz.length - 1;
-		//valor booleano indicando que o numero esta entre os vertices
+		// valor booleano indicando que o numero esta entre os vertices
 //		boolean entreVertices = coluna > 0 && coluna < ultimaLinhaColuna;
-		
+
 		if (coluna == 0) {
-			//o numero não tem numeros acima e a esquerda
+			// o numero não tem numeros acima e a esquerda
 			for (int i = 0; i < matriz.length; i++) {
 				for (int j = 0; j < matriz[i].length; j++) {
 					int vizinhoDireita = matriz[0][1];
@@ -175,8 +188,8 @@ public class PesquisasMatriz {
 					return new int[] { vizinhoDireita, vizinhoAbaixo };
 				}
 			}
-		}else if (coluna == ultimaLinhaColuna) {
-			//o numero não tem numeros acima e a direita
+		} else if (coluna == ultimaLinhaColuna) {
+			// o numero não tem numeros acima e a direita
 			for (int i = 0; i < matriz.length; i++) {
 				for (int j = 0; j < matriz[i].length; j++) {
 					int vizinhoEsquerda = matriz[0][1];
@@ -184,9 +197,9 @@ public class PesquisasMatriz {
 					return new int[] { vizinhoEsquerda, vizinhoAbaixo };
 				}
 			}
-			
-		}else {
-			
+
+		} else {
+
 			for (int i = 0; i < matriz.length; i++) {
 				for (int j = 0; j < matriz[i].length; j++) {
 					int vizinhoEsquerda = matriz[0][coluna - 1];
@@ -197,15 +210,15 @@ public class PesquisasMatriz {
 		}
 		return null;
 	}
-		
-	public static int[] retornaNumerosVizinhosBase(int[][] matriz,int linha, int coluna) {
-		
+
+	public static int[] retornaNumerosVizinhosBase(int[][] matriz, int linha, int coluna) {
+
 		int ultimaLinhaColuna = matriz.length - 1;
-		//valor booleano indicando que o numero esta entre os vertices
+		// valor booleano indicando que o numero esta entre os vertices
 //		boolean entreVertices = coluna > 0 && coluna < ultimaLinhaColuna;
-		
+
 		if (coluna == 0) {
-			//o numero não tem numeros abaixo e a esquerda
+			// o numero não tem numeros abaixo e a esquerda
 			for (int i = 0; i < matriz.length; i++) {
 				for (int j = 0; j < matriz[i].length; j++) {
 					int vizinhoDireita = matriz[ultimaLinhaColuna][1];
@@ -213,8 +226,8 @@ public class PesquisasMatriz {
 					return new int[] { vizinhoDireita, vizinhoAcima };
 				}
 			}
-		}else if (coluna == ultimaLinhaColuna) {
-			//o numero não tem numeros acima e a direita
+		} else if (coluna == ultimaLinhaColuna) {
+			// o numero não tem numeros acima e a direita
 			for (int i = 0; i < matriz.length; i++) {
 				for (int j = 0; j < matriz[i].length; j++) {
 					int vizinhoEsquerda = matriz[ultimaLinhaColuna][ultimaLinhaColuna - 1];
@@ -222,9 +235,9 @@ public class PesquisasMatriz {
 					return new int[] { vizinhoEsquerda, vizinhoAcima };
 				}
 			}
-			
-		}else {
-			
+
+		} else {
+
 			for (int i = 0; i < matriz.length; i++) {
 				for (int j = 0; j < matriz[i].length; j++) {
 					int vizinhoEsquerda = matriz[ultimaLinhaColuna][coluna - 1];
@@ -235,8 +248,6 @@ public class PesquisasMatriz {
 		}
 		return null;
 	}
-	
-
 
 	public static int selecionaNumero(Scanner sc) {
 		System.out.println("Digite um número");

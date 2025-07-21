a46191c1f94b564f4fa35be671758f96cd6b6ff1
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-21 08:16:48 -0300

    implementa toda o restante da logica da classe Worker

--- JavaCompleto/src/br/com/rolf/secao/secao12/exerciciosresolvidos01/entities/HourContract.java
+++ JavaCompleto/src/br/com/rolf/secao/secao12/exerciciosresolvidos01/entities/HourContract.java
@@ -8,9 +8,47 @@ public class HourContract {
 	private Double valuePerHour;
 	private Integer hours;
 	
+	public HourContract() {
+		
+	}
+	
+	public HourContract(Date date, Double valuePerHour, Integer hours) {
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+
+
+
 	public double totalvalue() {
 		
 		return valuePerHour * hours;
 	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+	
+	
 
 }
--- JavaCompleto/src/br/com/rolf/secao/secao12/exerciciosresolvidos01/entities/Worker.java
+++ JavaCompleto/src/br/com/rolf/secao/secao12/exerciciosresolvidos01/entities/Worker.java
@@ -1,37 +1,50 @@
 package br.com.rolf.secao.secao12.exerciciosresolvidos01.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.rolf.secao.secao12.exerciciosresolvidos01.entities.enums.WorkerLevel;
 
 public class Worker {
-	
+
 	private String name;
 	private WorkerLevel level;
 	private Double baseSalary;
-	
+
+	// acrescentando as associacoes
+	private Departament departament;
+	// no diagrama UML, mostra que essa classe tem varios contratos
+	// como são varios, eles ficarão numa lista
+	//quando a composicao for do tipo 'tem-muitos' não iniciar no construtor
+	//iniciar a lista vazia
+	private List<HourContract> contracts = new ArrayList<HourContract>();
+
 	public Worker() {
-		
+
 	}
 
-	public Worker(String name, WorkerLevel level, Double baseSalary) {
+	public Worker(String name, WorkerLevel level, Double baseSalary, Departament departament) {
 		this.name = name;
 		this.level = level;
 		this.baseSalary = baseSalary;
+		this.departament = departament;
 	}
 
-	
 	public void addContract(HourContract contract) {
-		
+		contracts.add(contract);
+
 	}
-	
+
 	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
 		
 	}
-	
+
 	public double income(Integer year, Integer month) {
-		
+
 		return 0;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -55,8 +68,18 @@ public class Worker {
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
 	
-	
-	
- 
+
 }

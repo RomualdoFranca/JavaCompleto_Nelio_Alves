7a8a59b9ef2818bd0dfe9afc4547001a1b2c91d5
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-23 06:05:53 -0300

    refatora metodo createClient() para retornar um objeto

--- JavaCompleto/src/br/com/rolf/secao/secao12/exerciciofixacao/entities/Program.java
+++ JavaCompleto/src/br/com/rolf/secao/secao12/exerciciofixacao/entities/Program.java
@@ -12,13 +12,16 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Instanciando um objeto Product");
-		Product p1 = createProduct(sc);
-		System.out.println(p1);
+//		Product p1 = createProduct(sc);
+//		System.out.println(p1);
 
+		System.out.println("\nInstanciando um objeto Client");
+		Client c1 = createClient(sc);
+		System.out.println(c1);
 		sc.close();
 
 	}
-	
+	//metodo para instanciar um objeto do tipo product
 	public static Product createProduct(Scanner sc) {
 		System.out.print("Product name: ");
 		String name = sc.nextLine();
@@ -28,6 +31,36 @@ public class Program {
 		return new Product(name, price);
 	}
 
+	//metodo para instanciar um objeto do tipo client
+	public static Client createClient(Scanner sc) {
+		System.out.println("Enter data client");
+		System.out.println("Name: ");
+		String name = sc.nextLine();
+
+		System.out.println("Email: ");
+		String email = sc.nextLine();
+
+		System.out.println("Birth date (DD/MM/YYYY): ");
+		String date = sc.nextLine();
+		LocalDate localDate = convertDate(date);
+		
+		return new Client(name, email, localDate);
+	}
+	//metodo para instanciar um objeto do tipo orderItems
+	public static OrderItem createOrderItems(Scanner sc) {
+		System.out.println("Enter order data:");
+		System.out.println("How many items to this order?");
+		int numberItems = sc.nextInt();
+		sc.nextLine();
+		
+		for (int i = 1; i <= numberItems; i++) {
+			System.out.println("Enter #" + i + " item data");
+			
+		}
+		
+	}
+	
+	
 	public static void readOrderItems(Scanner sc, OrderItem item, Product product) {
 		System.out.println("Enter order data:");
 		System.out.println("How many items to this order?");

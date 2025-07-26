8b944f3fe047fff04aaf606847e9ce92911a35ae
RomualdoFranca <romualdo.ads2022@gmail.com>
2025-07-26 13:28:26 -0300

    cria novo pacote para exercicios elaborados por IA

---
+++ JavaCompleto/src/br/com/rolf/ia/exercicios/composicao/entities/PrograminComposition.java
@@ -0,0 +1,10 @@
+package br.com.rolf.ia.exercicios.composicao.entities;
+
+public class PrograminComposition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
